cbdbb6710204094a656c931a225a817f44327cdc
Ankit-Sharma-17 <ankitkumarsharma145@gmail.com>
2026-02-18 00:51:30 +0530

    error solving

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio-Website/",
+
   server: {
     host: "::",
     port: 8080,
@@ -11,7 +13,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
